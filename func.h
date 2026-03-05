493cb884fa6b00c1dae78c573f6b86094f568d5f
STurtle <dexikonhack1450@gmail.com>
2026-03-05 19:30:15 +0100

    d

--- func.h
+++ func.h
@@ -10,7 +10,7 @@ void is_file_open(const FILE* f);
 int is_file_empty(const char* path);
 void mod_reader(int dat_el, int mod[], const char* path);
 void random_mod(int dat_el, int mod_arr[], const char* path);
-void ASCII_coder(const char* path, char data[], int dat_el, int mod_arr[], int mod_able);
-void ASCII_reader(const char* path, int dat_el, int mod_arr[], int mod_able);
+void ASCII_coder(const char* path, char data[], int dat_el, int mod_arr[]);
+void ASCII_reader(const char* path, int dat_el, int mod_arr[]);
 
 #endif
